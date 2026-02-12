1f935780fa43661c707abe833dc619511738f81c
Sophia Pouya <shp77@cornell.edu>
2026-02-12 15:27:47 -0600

    function for parsing brainvision files to get pulse array

--- helper_functions.py
+++ helper_functions.py
@@ -3,25 +3,37 @@ from tkinter import filedialog
 import sys
 from pathlib import Path
 import scipy.io as sio
+from scipy.io import wavfile
+import mne
+import os
 
-
-# pulls up the file explorer to select the preprocessed file
-def select_file():
+# pulls up the file explorer 
+def select_file(title, filetype):
     root = tk.Tk()
     root.withdraw()
     root.attributes('-topmost', True)
-    file_path = filedialog.askopenfile(
-        title="Select a preprocessed file",
-        filetypes=[("MAT Files", "*.mat")]
+    file_path = filedialog.askopenfilename(
+        title=title,
+        filetypes=filetype
     )
     root.destroy()
     if file_path is None:
-        print("No file selected. You must select a preprocessed .mat file")
+        print(f"No file selected for {filetype[0][0]}")
         sys.exit(0)
-    return Path(file_path)
+    return Path(str(file_path))
 
-# will grab the metadata from matlab file necessary for rest of pipeline
-def get_metadata(file_path):
-    metadata= {}
-    file_contents = sio.loadmat(file_path)
+# parse the brain vision vhdr and vmrk file to get the pulses array and sampling frequency 
+def parse_brainvision(vhdr_path_object):
+    raw = mne.io.read_raw_brainvision(vhdr_path_object, preload=False)
+    sfreq = raw.info['sfreq']
+    events, event_id = mne.events_from_annotations(raw)
+    # event_id -> {'Reponse name': number of responses }
+    # events -> [sample_index, 0, event_code]
+    r257_code = event_id['Response/R257']
+    r257_rows = events[events[:,2] == r257_code]
+    r257_samples = r257_rows[:,0]
+    
+    return(sfreq, r257_samples)
+
+
 
