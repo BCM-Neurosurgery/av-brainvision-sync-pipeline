abfb059186127218b741f888dbe1edf839145793
Sophia Pouya <shp77@cornell.edu>
2026-02-16 21:21:54 -0600

    adding the most recent pipeline

--- audio_eeg_sync.py
+++ audio_eeg_sync.py
@@ -1,4 +1,5 @@
 from helper_functions import *
+from cli_nbu_mvp import run_pipeline_gui
 
 # pull up the file explorer and get the file path for the preprocessed mat file 
 # pull up the vmrk file 
@@ -24,7 +25,7 @@ from helper_functions import *
 # find the line of best fit to predict 
 
 if __name__ == "__main__":
-   
+    
     # pull up the file explorer to choose the preprocessed file
     vhdr_path_obj = select_file(
         title="Select a brain vision .vhdr file",
@@ -34,6 +35,19 @@ if __name__ == "__main__":
         title="Select the audio .wav file with sync pulses (greatestNumber-*.wav)",
         filetype=[("Audio file", "*.wav")]
     )
+    video_path_obj = select_dir(
+        title="Select the directory containing the video files you want to align"
+    )
+
+    output_path_obj =select_dir(
+        title="Select the directory you want to save the aligned files to"
+    )
+
+    # # for testing -> manual directory paths
+    # vhdr_path_obj= Path("/Users/sophiapouya/Desktop/EEG/PRT.vhdr")
+    # audio_path_obj = Path("/Users/sophiapouya/Desktop/prt_av/AUDIO/20260212/Media/04-260212_1832.wav")
+    # video_path_obj = Path("/Users/sophiapouya/Desktop/prt_av/VIDEO/20260212")
+    # output_path_obj=Path("/Users/sophiapouya/workspace/bcm/av-brainvision-sync-pipeline/output")
 
     # get the exp start and end time and sync pulses from the brain vision file
     exp_start_time, exp_end_time, bv_sync_pulse_array = parse_brainvision(vhdr_path_object=vhdr_path_obj)
@@ -47,6 +61,22 @@ if __name__ == "__main__":
     # find the start and end sample for audio 
     audio_start_sample = apply_alignment(rel_clock_rates=rel_clock_rates, offset=offset, bv_time=exp_start_time, audio_sfreq=audio_sfreq)
     audio_end_sample = apply_alignment(rel_clock_rates=rel_clock_rates, offset=offset, bv_time=exp_end_time, audio_sfreq=audio_sfreq)
+
+    print("rel_clock_rates:", rel_clock_rates)
+    print("offset:", offset)
+    print("audio_start_sample:", audio_start_sample)
+    print("audio_end_sample:", audio_end_sample)
+
+    # run the audio video sync pipeline 
+    audio_dir = audio_path_obj.parent
+    run_pipeline_gui(
+    audio_dir=audio_dir,
+    video_dir=video_path_obj,
+    out_dir=output_path_obj,
+    site="jamail",
+    audio_sample_start=audio_start_sample,
+    audio_sample_end=audio_end_sample,
+)
 
 
 
